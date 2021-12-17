979604a002ca0481976ae2f49cff7627035e2936
LorenzoMolteni <lorenzomolteni2@gmail.com>
2021-12-17 13:04:43 +0100

    showing max 3 categories in game card

--- planb/src/components/GameCard.js
+++ planb/src/components/GameCard.js
@@ -12,14 +12,22 @@ function GameCard(props) {
         minutes: parseInt(props.game.Duration.split(':')[1]),
         difficulty: props.game.Difficulty
     }
+    //making the time better to display to users
     let time;
     if(game.hour === 0)
         time = game.minutes+"m";
     else
         time = game.hour+"h"+game.minutes+"m";
 
-    let subtitle = game.categories.join(' | ');
+    //displaying game categories (max 3)
+    let subtitle;
+    if(game.categories.length < 4)
+        subtitle = game.categories.join(' | ');
+    else {
+        subtitle = game.categories[0] + ' | ' + game.categories[1] + ' | '  + game.categories[2] + ' | +' + (game.categories.length-3); 
+    }
 
+    //choosing the dice icon depending on game difficulty
     let diceIcon;
     if(game.difficulty === 'Easy')
         diceIcon = <Dice1 className='mx-2'/>;
