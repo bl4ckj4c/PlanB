43d3b285da8d006378fa1f9079f01c9cf4380b4a
Dario Lanfranco <lanfranco.dario@gmail.com>
2021-12-19 14:34:24 +0100

    Added pdf implementation files.

--- planb/src/components/Rules.js
+++ planb/src/components/Rules.js
@@ -1,6 +1,21 @@
+//import PDF from './PDF';
+//import PDFView from "./PDFView";
+import PDFViewer from 'pdf-viewer-reactjs'
+
 function Rules(props) {
+    const {} = props;
+
+    const pdf_url = "http://www.africau.edu/images/default/sample.pdf";
+
+    //const prevent_cors_errors = "https://cors-anywhere.herokuapp.com/";
+    //const complete_url = prevent_cors_errors + pdf_url;
+
     return (
-        <></>
+        <>
+            <PDFViewer document={{
+                url: pdf_url,
+            }}/>
+        </>
     );
 }
 
