4ae6fd52455faf679f07d1a5f5243f945b386261
bl4ckj4c <jacopo.marino.1998@gmail.com>
2021-12-20 10:24:48 +0100

    Fixed ProfileInfo

--- planb/src/components/ProfileInfo.js
+++ planb/src/components/ProfileInfo.js
@@ -1,62 +1,85 @@
-import {Button, Col, Container, Row, Placeholder} from "react-bootstrap";
+import {Button, Col, Container, Row, Placeholder, Spinner} from "react-bootstrap";
 import {ChevronLeft, PersonCircle} from "react-bootstrap-icons";
-import React from "react";
+import React, {useState} from "react";
 import API from '../API'
+import {Navigate} from "react-router-dom";
 
 function ProfileInfo(props) {
+    const [page, setPage] = useState('');
+
     const handleLogout = (event) => {
         API.signOutUser()
-            .then(() => props.setIsSignedIn(false))
+            .then(() => {
+                props.setIsSignedIn(false);
+                setPage('login');
+            })
             .catch((error) => console.log(error));
+    }
+
+    const handleBackButton = (event) => {
+        setPage('mygames');
     }
 
     return (
         <>
-            <Container id="nav" className="pb-2">
-                <Row className='justify-content-between mt-2'>
-                    <a href='/mygames'>
-                        <Col xs={6}>
-                            <ChevronLeft size={25} color="grey"/>
-                            <span className="text-muted align-middle">Back</span>
-                        </Col>
-                    </a>
-                </Row>
-            </Container>
-            <Container>
-                <Row className='m-5 text-muted'>
-                    <PersonCircle size={90}/>
-                </Row>
-                <Row className='align-items-center'>
-                    <Col className='text-muted text-center'>
-                        Email
-                    </Col>
-                </Row>
-                <Row className='align-items-center'>
-                    <Col className='text-center'>
-                        {props.userLoading ?
-                            <Placeholder as="p" animation="glow">
-                                <Placeholder bg='secondary' xs={4}/>
-                            </Placeholder>
-                            :
-                            props.user.email
-                        }
-                    </Col>
-                </Row>
-                <Row className='fixed-bottom mx-4 mb-4'>
-                    {props.userLoading ?
-                        <Placeholder animation="glow">
-                            <Placeholder.Button xs={12} bg='danger' aria-hidden="true"/>
-                        </Placeholder>
-                        :
-                        <Button
-                            variant="danger"
-                            onClick={() => handleLogout()}>
-                            Log out
-                        </Button>
-                    }
-                </Row>
-            </Container>
+            {page !== '' && <Navigate replace to={`/${page}`}/>}
+            {
+                <>
+                    <Container id="nav" className="pb-2">
+                        <Row className='justify-content-between mt-2'>
+                            <Col xs={6}>
+                                <Button
+                                    type="submit"
+                                    className="bg-white border-0 p-0"
+                                    onClick={() => handleBackButton()}>
+                                    <ChevronLeft size={25} color="grey"/>
+                                    <span className="text-muted align-middle">Back</span>
+                                </Button>
+                            </Col>
+                        </Row>
+                    </Container>
+                    <Container>
+                        <Row className='m-5 text-muted'>
+                            <PersonCircle size={90}/>
+                        </Row>
+                        <Row className='align-items-center'>
+                            <Col className='text-muted text-center'>
+                                Email
+                            </Col>
+                        </Row>
+                        <Row className='align-items-center'>
+                            <Col className='text-center'>
+                                {props.userLoading ?
+                                    <Placeholder as="p" animation="glow">
+                                        <Placeholder bg='secondary' xs={4}/>
+                                    </Placeholder>
+                                    :
+                                    props.user.email
+                                }
+                            </Col>
+                        </Row>
+                        <Row className='fixed-bottom mx-4 mb-4'>
+                                <Button
+                                    variant="danger"
+                                    onClick={() => handleLogout()}
+                                    disabled={props.userLoading}>
+                                    {props.userLoading ?
+                                        <Spinner
+                                            as="span"
+                                            animation="border"
+                                            size="sm"
+                                            role="status"
+                                            aria-hidden="true"/>
+                                        :
+                                        'Log out'
+                                    }
+                                </Button>
+                        </Row>
+                    </Container>
+                </>
+            }
         </>
+
     );
 }
 
