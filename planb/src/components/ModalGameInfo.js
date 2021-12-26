fcc1484ea012a517dcf0c24b1eb42aed04172839
bl4ckj4c <jacopo.marino.1998@gmail.com>
2021-12-26 12:29:08 +0100

    Delete game confirm modal

--- planb/src/components/ModalGameInfo.js
+++ planb/src/components/ModalGameInfo.js
@@ -8,6 +8,7 @@ import React, {useState} from "react";
 
 function ModalGameInfo(props) {
     const [buttonLoading, setButtonLoading] = useState(false);
+    const [showDeleteWarning, setShowDeleteWarning] = useState(false);
 
     //props.add === "true"
     const game = props.game;
@@ -46,62 +47,76 @@ function ModalGameInfo(props) {
     }
 
     return (
-        <Modal
-            {...props}
-            size="lg"
-            aria-labelledby="contained-modal-title-vcenter"
-            centered
-        >
-            <Modal.Header closeButton>
-                <Modal.Title id="contained-modal-title-vcenter">
-                    {game.Title}
-                </Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-                <GameInfo game={game}/>
-            </Modal.Body>
-            <Modal.Footer as='div'>
-                {props.add === "true" ?
-                    <Button
-                        className='mx-1'
-                        style={{
-                            width: '100%'
-                        }}
-                        variant="success"
-                        onClick={() => addGame()}>
-                        {buttonLoading ?
-                            <Spinner
-                                as="span"
-                                animation="border"
-                                size="sm"
-                                role="status"
-                                aria-hidden="true"/>
-                            :
-                            'Add'
-                        }
-                    </Button>
-                    :
-                    <Button
-                        className='mx-1'
-                        style={{
-                            width: '100%'
-                        }}
-                        variant="danger"
-                        onClick={() => deleteGame()}>
-                        {buttonLoading ?
-                            <Spinner
-                                as="span"
-                                animation="border"
-                                size="sm"
-                                role="status"
-                                aria-hidden="true"/>
-                            :
-                            'Delete'
-                        }
-                    </Button>
-                }
-            </Modal.Footer>
-        </Modal>
+        <>
+            <Modal
+                {...props}
+                size="lg"
+                aria-labelledby="contained-modal-title-vcenter"
+                centered
+            >
+                <Modal.Header closeButton>
+                    <Modal.Title id="contained-modal-title-vcenter">
+                        {game.Title}
+                    </Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <GameInfo game={game}/>
+                </Modal.Body>
+                <Modal.Footer as='div'>
+                    {props.add === "true" ?
+                        <Button
+                            className='mx-1'
+                            style={{
+                                width: '100%'
+                            }}
+                            variant="success"
+                            onClick={() => addGame()}>
+                            {buttonLoading ?
+                                <Spinner
+                                    as="span"
+                                    animation="border"
+                                    size="sm"
+                                    role="status"
+                                    aria-hidden="true"/>
+                                :
+                                'Add'
+                            }
+                        </Button>
+                        :
+                        <Button
+                            className='mx-1'
+                            style={{
+                                width: '100%'
+                            }}
+                            variant="danger"
+                            onClick={() => setShowDeleteWarning(true)}>
+                            {buttonLoading ?
+                                <Spinner
+                                    as="span"
+                                    animation="border"
+                                    size="sm"
+                                    role="status"
+                                    aria-hidden="true"/>
+                                :
+                                'Delete'
+                            }
+                        </Button>
+                    }
+                </Modal.Footer>
+            </Modal>
+            <Modal
+                size="lg"
+                aria-labelledby="contained-modal-title-vcenter"
+                centered
+                show={showDeleteWarning}
+                onHide={() => setShowDeleteWarning(false)}>
+                <Modal.Header closeButton>
+                    <Modal.Title id="contained-modal-title-vcenter">
+                        Are you sure to delete this game from your collection?
+                    </Modal.Title>
+                </Modal.Header>
+            </Modal>
+        </>
     );
 }
 
